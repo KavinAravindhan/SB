f4c67d720e4d13d89a3929239837631716849c62
KavinAravindhan <kavin.aravindhan@gmail.com>
2022-12-31 19:50:09 +0530

    Header Updated

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 <Navbar />
                 <div className='header-content flex flex-c text-center text-white'>
                     <h2 className='header-title text-capitalize'>find your book of choice.</h2><br />
-                    <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
+                    <p className='header-text fs-18 fw-3'>Between The Pages Of A Book Is A Lovely Place to Be</p>
                     <SearchForm />
                 </div>
             </header>
@@ -19,3 +19,5 @@ const Header = () => {
 }
 
 export default Header
+
+
